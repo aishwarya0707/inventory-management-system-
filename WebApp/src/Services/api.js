4c52399d128361b637539c5b00a563ca0a46f434
aishwarya3431 <aishwarya3431@gmail.com>
2026-06-05 12:50:05 +0530

    Update Axios to use VITE_API_URL environment variable

--- WebApp/src/Services/api.js
+++ WebApp/src/Services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// This points to your running Flask server
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
 
 export const api = axios.create({
   baseURL: API_URL,
